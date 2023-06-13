f2a975f99ce1391b71c5e7ad09c131e70e858b8e
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-06-13 16:01:43 +1000

    isDarkMode prop passed down through app, functionality handled here for buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,38 +8,32 @@ import Info from './pages/Info'
 function App() {
   
   const [isDarkMode, setIsDarkMode] = useState(true);
-  const [buttonTextColor, setButtonTextColor] = useState('text-dark-mode-color');
-  const [borderColor, setBorderColor] = useState('border-dark-mode-color');
 
   const handleDarkMode = () => {
     setIsDarkMode(true);
-    setButtonTextColor('text-dark-mode-color');
-    setBorderColor('border-dark-mode-color');
   };
 
   const handleLightMode = () => {
     setIsDarkMode(false);
-    setButtonTextColor('text-light-mode-color');
-    setBorderColor('border-light-mode-color');
   };
 
   return (
-    <div className="bg-light-mode-color min-h-screen">
-      <div className="absolute inset-2 sm:inset-3 md:inset-4 lg:inset-5 border border-dark-mode-color">
+    <div className={isDarkMode ? "bg-light-mode-color min-h-screen" : "bg-dark-mode-color min-h-screen"}>
+      <div className={`absolute inset-2 sm:inset-3 md:inset-4 lg:inset-5 border ${isDarkMode ? "border-dark-mode-color" : "border-light-mode-color"}`}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/info" element={<Info />} />
+        <Route path="/" element={<Home isDarkMode={isDarkMode} />} />
+          <Route path="/projects" element={<Projects isDarkMode={isDarkMode} />} />
+          <Route path="/info" element={<Info isDarkMode={isDarkMode} />} />
         </Routes>
         <div className="fixed font-roboto-mono bottom-5 left-7 sm:bottom-6 sm:left-9 md:bottom-7 md:left-10 lg:bottom-8 lg:left-12 lg:text-lg">
           <button
-            className={`${buttonTextColor} mr-5 transition-transform hover:scale-110`}
+            className={`mr-5 transition-transform hover:scale-110 ${isDarkMode ? "text-dark-mode-color" : "text-light-mode-color"}`}
             onClick={handleLightMode}
           >
             {isDarkMode ? '□' : '■'} light
           </button>
           <button
-            className={`${buttonTextColor} transition-transform hover:scale-110`}
+            className={`transition-transform hover:scale-110 ${isDarkMode ? "text-dark-mode-color" : "text-light-mode-color"}`}
             onClick={handleDarkMode}
           >
             {isDarkMode ? '■' : '□'} dark
@@ -51,3 +45,5 @@ function App() {
 }
 
 export default App
+
+
