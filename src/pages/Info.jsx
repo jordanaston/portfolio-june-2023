887e2361b9fa46e6d06dd487a55e2ef0d1161f2f
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-06-13 12:37:59 +1000

    header and footer added to info page

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import HomeContent from '../components/HomeContent';
 
 function Info() {
-  return (
-    <div>Info</div>
-  )
-}
+    return (
+      <div className="flex flex-col h-full">
+          <Header />
+          <div className="text-dark-mode-color flex-grow overflow-auto">
+            
+          </div>
+          <Footer />
+      </div>
+    )
+  }
 
 export default Info
