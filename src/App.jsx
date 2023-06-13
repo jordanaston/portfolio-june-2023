fc4f613436931babf33e1121130a1c59ddd282cf
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-06-13 14:55:11 +1000

    light and dark mode buttons added to App.jsx for prop passing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,23 @@ import Info from './pages/Info'
 
 
 function App() {
+  
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [buttonTextColor, setButtonTextColor] = useState('text-dark-mode-color');
+  const [borderColor, setBorderColor] = useState('border-dark-mode-color');
+
+  const handleDarkMode = () => {
+    setIsDarkMode(true);
+    setButtonTextColor('text-dark-mode-color');
+    setBorderColor('border-dark-mode-color');
+  };
+
+  const handleLightMode = () => {
+    setIsDarkMode(false);
+    setButtonTextColor('text-light-mode-color');
+    setBorderColor('border-light-mode-color');
+  };
+
   return (
     <div className="bg-light-mode-color min-h-screen">
       <div className="absolute inset-2 sm:inset-3 md:inset-4 lg:inset-5 border border-dark-mode-color">
@@ -14,6 +31,20 @@ function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/info" element={<Info />} />
         </Routes>
+        <div className="fixed font-roboto-mono bottom-5 left-7 sm:bottom-6 sm:left-9 md:bottom-7 md:left-10 lg:bottom-8 lg:left-12 lg:text-lg">
+          <button
+            className={`${buttonTextColor} mr-5 transition-transform hover:scale-110`}
+            onClick={handleLightMode}
+          >
+            {isDarkMode ? '□' : '■'} light
+          </button>
+          <button
+            className={`${buttonTextColor} transition-transform hover:scale-110`}
+            onClick={handleDarkMode}
+          >
+            {isDarkMode ? '■' : '□'} dark
+          </button>
+        </div>
       </div>
     </div>
   );
