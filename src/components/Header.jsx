ac071b94a4c64d5a752ac65ef46e85cf7d80f403
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-06-13 13:12:37 +1000

    mobile menu now visible on size condition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import CodeSymbol from './CodeSymbol'
 import TopNavBar from './TopNavBar'
+import MobileMenu from './MobileMenu'
 
 function Header() {
   return (
     <div>
         <div className="flex justify-between text-dark-mode-color border border-dark-mode-color">
             <CodeSymbol />
-            <TopNavBar />
+            <div className="md:hidden">
+                <MobileMenu />
+            </div>
+            <div className="hidden md:block">
+                <TopNavBar />
+            </div>
         </div>
     </div>
   )
---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import MobileMenuDark from '../assets/mobile-menu-dark.png';
+
+function MobileMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
+
+  const linkStyles = {
+    fontSize: '2rem',
+    transition: 'font-size 0.3s',
+  };
+
+  const linkHoverStyles = {
+    fontSize: '2.3rem',
+  };
+
+  return (
+    <div className="relative">
+      <button onClick={handleClick}>
+        <img 
+          src={MobileMenuDark} 
+          alt="Mobile Menu Image" 
+          className="w-16 pt-1 sm:pt-3 sm:mr-3" 
+        />
+      </button>
+      <div
+        className={`
+          fixed inset-0 flex items-center justify-start z-50 transition-transform duration-300 bg-light-mode-color
+          ${isOpen ? 'translate-x-0' : '-translate-x-full'}
+        `}
+      >
+        <div className="absolute top-0 right-0 h-full p-4 flex flex-col items-end">
+          <button onClick={handleClick} className="text-4xl text-dark-mode-color">
+            X
+          </button>
+        </div>
+        <ul className="flex flex-col items-start ml-4 font-roboto-mono justify-center h-full space-y-10">
+          <li>
+            <Link
+              to="/"
+              onClick={handleClick}
+              className="block text-5xl font-light ml-2 text-dark-mode-color"
+              style={linkStyles}
+              onMouseOver={(e) => { e.target.style.fontSize = linkHoverStyles.fontSize; }}
+              onMouseOut={(e) => { e.target.style.fontSize = linkStyles.fontSize; }}
+            >
+              ■ Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/projects"
+              onClick={handleClick}
+              className="block text-5xl font-light ml-2 text-dark-mode-color"
+              style={linkStyles}
+              onMouseOver={(e) => { e.target.style.fontSize = linkHoverStyles.fontSize; }}
+              onMouseOut={(e) => { e.target.style.fontSize = linkStyles.fontSize; }}
+            >
+              ■ Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/info"
+              onClick={handleClick}
+              className="block text-5xl font-light ml-2 text-dark-mode-color"
+              style={linkStyles}
+              onMouseOver={(e) => { e.target.style.fontSize = linkHoverStyles.fontSize; }}
+              onMouseOut={(e) => { e.target.style.fontSize = linkStyles.fontSize; }}
+            >
+              ■ Info
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default MobileMenu;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
