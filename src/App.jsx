62543f2790ee6218ae1d41907370991a5cfdc397
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-06-13 11:06:23 +1000

    border and background applied to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import React, { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Info from './pages/Info'
+// import ParticleBackground from './components/ParticleBackground'
+
+
 
 function App() {
-  
-
   return (
-    <div>
-      <h1 className="font-capture-waves text-3xl">Hello World</h1>
+    <div className="bg-light-mode-color min-h-screen">
+      <div className="absolute inset-5 border border-dark-mode-color p-3">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/info" element={<Info />} />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
