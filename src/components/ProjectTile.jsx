bbf38ad8b16c81705e71dd42a3eb30e242e844e5
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-06-16 15:26:46 +1000

    project tiles now work as a link to the project details route

--- src/components/ProjectTile.jsx
+++ src/components/ProjectTile.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function ProjectTile({ projectDescription, projectDetails, techStack, projectImage, isDarkMode }) {
+function ProjectTile({ projectId, projectDescription, projectDetails, techStack, projectImage, isDarkMode }) {
   return (
     <div>
-      <div className="m-4 w-80 h-60">
-        {projectImage ? (
-          <img src={projectImage} alt="Project Image" />
-        ) : (
-          <div className={`flex justify-center items-center border w-80 h-60 font-roboto font-regular ${isDarkMode ? 'border-dark-mode-color' : 'border-light-mode-color'}`}>
-            <p>In Development...</p>
-          </div>
-        )}
-      </div>
+      <Link to={`/projects/${projectId}`}>
+        <div className="m-4 w-80 h-60">
+          {projectImage ? (
+            <img src={projectImage} alt="Project Image" />
+          ) : (
+            <div className={`flex justify-center items-center border w-80 h-60 font-roboto font-regular ${isDarkMode ? 'border-dark-mode-color' : 'border-light-mode-color'}`}>
+              <p>In Development...</p>
+            </div>
+          )}
+        </div>
+      </Link>
       <div className="font-roboto font-light">
         <p className="m-5">{projectDetails}</p>
         <p className="m-5 text-xl">{projectDescription}</p>
