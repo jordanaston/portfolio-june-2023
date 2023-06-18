c352abc6c4e9cb7861412e0db4e93eaff495aaf0
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-06-18 20:48:23 +1000

    config file updated to handle css purge error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  purge: {
-    options: {
-      safelist: [
-        'dark-mode-color', 
-        'light-mode-color',
-        'border-dark-mode-color',
-        'border-light-mode-color',
-        'text-dark-mode-color',
-        'text-light-mode-color'
-      ],
-    }
-  },
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
         'dark-mode-color': 'rgb(163,163,163)',
-        'light-mode-color': 'rgb(13,13,13)'
+        'light-mode-color': 'rgb(13,13,13)',
       },
       spacing: {
         '30': '30px',
@@ -33,11 +24,21 @@ export default {
         'light': 300,
         'regular': 400,
         'medium': 500,
-        'bold': 700
+        'bold': 700,
       },
     },
   },
   plugins: [],
+  safelist: [
+    'dark-mode-color', 
+    'light-mode-color',
+    'border-dark-mode-color',
+    'border-light-mode-color',
+    'text-dark-mode-color',
+    'text-light-mode-color',
+  ],
 };
+
+
 
 
