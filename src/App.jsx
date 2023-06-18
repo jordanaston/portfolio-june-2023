ca5855d1c93e54aea23172186923ceb218130281
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-06-18 18:21:55 +1000

    context added to App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import Info from './pages/Info'
 import ProjectDetails from './components/ProjectDetails';
 import ParticleBackground from './components/ParticleBackground'
 import ProjectContext from './components/ProjectContext';
-import PlantAppImage from './assets/plantapp-image.png';
-import VinylDataImage from './assets/vinyl-data-image.png';
 
 function App() {
 
@@ -21,39 +19,8 @@ function App() {
     setIsDarkMode(false);
   };
 
-  const projectItems = [
-    {
-      id: 'full-stack-app',
-      details: '2023 / Coder Academy',
-      description: 'Full Stack Application',
-      stack: 'MERN Stack | Tailwind',
-      image: ''
-    },
-    {
-      id: 'music-player-app',
-      details: '2023 / Personal Project',
-      description: 'Music Player App',
-      stack: 'Javascript | React | Tailwind',
-      image: ''
-    },
-    {
-      id: 'vinyl-data-api',
-      details: '2023 / Coder Academy',
-      description: 'Vinyl Data - API Webserver',
-      stack: 'Python | Flask | PostgreSQL',
-      image: VinylDataImage,
-    },
-    {
-      id: 'plantapp',
-      details: '2022 / Coder Academy',
-      description: 'PlantApp - Terminal Application',
-      stack: 'Python',
-      image: PlantAppImage,
-    },
-  ];
-
   return (
-    <ProjectContext.Provider value={projectItems}>
+    <ProjectContext.Provider value={ProjectContext._currentValue}>
       <div className={isDarkMode ? "bg-light-mode-color min-h-screen" : "bg-dark-mode-color min-h-screen"}>
         <ParticleBackground isDarkMode={isDarkMode} />
         <div className={`absolute inset-2 sm:inset-3 md:inset-4 lg:inset-5 border ${isDarkMode ? "border-dark-mode-color" : "border-light-mode-color"}`}>
@@ -84,6 +51,8 @@ function App() {
 }
 
 export default App;
+
+
 
 
 
