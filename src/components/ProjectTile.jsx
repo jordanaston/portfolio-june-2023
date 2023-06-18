013acd4dfe61964b4ce8134f8a3150e2cb962666
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-06-18 18:21:13 +1000

    switched to context to handle the deatils for the project tiles

--- src/components/ProjectTile.jsx
+++ src/components/ProjectTile.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import ProjectContext from '../components/ProjectContext';
 
-function ProjectTile({ projectId, projectDescription, projectDetails, techStack, projectImage, isDarkMode }) {
+function ProjectTile({ projectId, isDarkMode }) {
+
+  const projectItems = useContext(ProjectContext);
+  const project = projectItems.find((item) => item.id === projectId);
+
+  if (!project) {
+    return null;
+  }
+
   return (
     <div>
       <Link to={`/projects/${projectId}`}>
         <div className="m-4 w-80 h-60 hover:opacity-75 transition-opacity duration-200">
-          {projectImage ? (
-            <img src={projectImage} alt="Project Image" />
+          {project.image ? (
+            <img src={project.image} alt="Project Image" />
           ) : (
             <div className={`flex justify-center items-center border w-80 h-60 font-roboto font-regular ${isDarkMode ? 'border-dark-mode-color' : 'border-light-mode-color'}`}>
               <p>In Development...</p>
@@ -16,13 +25,14 @@ function ProjectTile({ projectId, projectDescription, projectDetails, techStack,
         </div>
       </Link>
       <div className="font-roboto font-light">
-        <p className="m-5">{projectDetails}</p>
-        <p className="m-5 text-xl">{projectDescription}</p>
-        <p className="m-5 text-md">{techStack}</p>
+        <p className="m-5">{project.details}</p>
+        <p className="m-5 text-xl">{project.description}</p>
+        <p className="m-5 text-md">{project.stack}</p>
       </div>
     </div>
   );
 }
 
 export default ProjectTile;
+
 
